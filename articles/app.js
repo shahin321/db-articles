d75c5b6161d1d048e7dfc1258f0c68e78c7a1dcd
Choudhury <shahinchoudhurypro@gmail.com>
2019-05-26 10:36:32 +0200

    Fix erreur image

--- articles/app.js
+++ articles/app.js
@@ -61,24 +61,29 @@ function myFunction(id){
       fetch('https://my-json-server.typicode.com/shahin321/dbarticles/articles')
         .then( response => response.json() )
         .then( articles => {
-			if (articles){
-				for(let article of articles){
-					let clone = template.cloneNode(true);
-					clone.querySelector('[article-title]').innerText = article.title;
-					clone.querySelector('[article-content]').innerText = article.content;
-					clone.querySelector('[article-img]').setAttribute("src", article.img );
-					var input = document.createElement('input');
-					input.setAttribute("type", "hidden")
-					input.id = 'input_' + article.id ;
-					input.value = article.id ;
-					var button = document.createElement('input');
-					button.setAttribute('type', 'submit');
-					button.innerHTML = 'Lire plus';
-					button.addEventListener('click', function(){myFunction(article.id)});
-					document.querySelector('#articles').appendChild(clone);
-					document.querySelector('#articles').appendChild(input);
-					document.querySelector('#articles').appendChild(button);
-				}
+
+		if (articles){
+			for(let article of articles){
+				let clone = template.cloneNode(true);
+				clone.querySelector('[article-title]').innerText = article.title;
+				clone.querySelector('[article-content]').innerText = article.content;
+				clone.querySelector('[article-image]').setAttribute("src", article.image );
+				var input = document.createElement('input');
+				input.setAttribute("type", "hidden")
+				input.id = 'input_' + article.id ;
+				input.value = article.id ;
+				var button = document.createElement('input');
+				button.setAttribute('type', 'submit');
+				button.innerHTML = 'Lire plus';
+				button.addEventListener('click', function(){myFunction(article.id)});
+				document.querySelector('#articles').appendChild(clone);
+				document.querySelector('#articles').appendChild(input);
+				document.querySelector('#articles').appendChild(button);
 			}
+		}
+
         });
     });
+
+
+
