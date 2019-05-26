7deefb54e1bfbf7229481ca9c956a86fdf213bb5
Choudhury <shahinchoudhurypro@gmail.com>
2019-05-26 05:00:53 +0200

    Fix app.js

--- articles/app.js
+++ articles/app.js
@@ -49,7 +49,7 @@
                for (let article of articles) {
                     let clone = template.cloneNode(true);
                     clone.querySelector('[article-title]').innerText = article.title;
-                    clone.querySelector('[article-image]').setattribute("src", article.image);
+                    clone.querySelector('[article-image]').setAttribute("src", article.image );
                     clone.querySelector('[article-excerpt]').innerText = article.excerpt;
                         //crée un clone
                     document.querySelector('#articles').appendChild(clone);
@@ -58,35 +58,3 @@
 
         });
     });
-
-
-
- fetch('article.html.tpl')
-.then(response => response.text())
-.then(templateString => {
-let parserHTML = new DOMParser();
-let template = parserHTML.parseFromString(templateString, 'text/html').body.firstChild;
-
-     fetch('https://my-json-server.typicode.com/shahin321/dbarticles/articles')
-	.then(response => response.json())
-	.then(articles => {
-		if (articles){
-			for(let article of articles){
-				let clone = template.cloneNode(true);
-				clone.querySelector('[article-title]').innerText = article.title;
-				clone.querySelector('[article-image]').setAttribute("src", article.image);
-				var input = document.createElement('input');
-				input.setAttribute("type", "hidden")
-				input.id = 'input_' + article.id ;
-				input.value = article.id ;
-				var button = document.createElement('input');
-				button.setAttribute('type', 'submit');
-				button.innerHTML = 'Lire plus';
-				button.addEventListener('click', function(){myFunction(article.id)});
-				document.querySelector('#articles').appendChild(clone);
-				document.querySelector('#articles').appendChild(input);
-				document.querySelector('#articles').appendChild(button);
-			}
-		}
-	});
-});
