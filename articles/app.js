e53d98a4a513b82fafb0c72b68ed0251c7a4a0b0
Choudhury <shahinchoudhurypro@gmail.com>
2019-05-26 11:13:16 +0200

    Update

--- articles/app.js
+++ articles/app.js
@@ -33,7 +33,7 @@
 
 //FETCH
 function myFunction(id){
-  fetch('content-article.html.tpl')
+  fetch('content_article.html.tpl')
   .then(response => response.text())
   .then(templateString => {
     let parserHTML = new DOMParser();
@@ -62,25 +62,25 @@ function myFunction(id){
         .then( response => response.json() )
         .then( articles => {
 
-		if (articles){
-			for(let article of articles){
-				let clone = template.cloneNode(true);
-				clone.querySelector('[article-title]').innerText = article.title;
-				clone.querySelector('[article-content]').innerText = article.content;
-				clone.querySelector('[article-image]').setAttribute("src", article.image );
-				var input = document.createElement('input');
-				input.setAttribute("type", "hidden")
-				input.id = 'input_' + article.id ;
-				input.value = article.id ;
-				var button = document.createElement('button');
-				button.setAttribute('type', 'button');
-				button.innerHTML = 'Lire plus';
-				button.addEventListener('click', function(){myFunction(article.id)});
-				document.querySelector('#articles').appendChild(clone);
-				document.querySelector('#articles').appendChild(input);
-				document.querySelector('#articles').appendChild(button);
-			}
-		}
+      		if (articles){
+      			for(let article of articles){
+      				let clone = template.cloneNode(true);
+      				clone.querySelector('[article-title]').innerText = article.title;
+      				clone.querySelector('[article-content]').innerText = article.content;
+      				clone.querySelector('[article-image]').setAttribute("src", article.image );
+      				var input = document.createElement('input');
+      				input.setAttribute("type", "hidden")
+      				input.id = 'input_' + article.id ;
+      				input.value = article.id ;
+      				var button = document.createElement('input');
+      				button.setAttribute('type', 'button');
+      				button.innerHTML = 'Lire plus';
+      				button.addEventListener('click', function(){myFunction(article.id)});
+      				document.querySelector('#articles').appendChild(clone);
+      				document.querySelector('#articles').appendChild(input);
+      				document.querySelector('#articles').appendChild(button);
+      			}
+      		}
 
         });
     });
