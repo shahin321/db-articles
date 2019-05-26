c7f70377a082cbe5353b879962b8e7fbbafcb18b
Choudhury <shahinchoudhurypro@gmail.com>
2019-05-26 03:21:02 +0200

    Update images and app.js

--- articles/app.js
+++ articles/app.js
@@ -4,7 +4,7 @@
 let parserHTML = new DOMParser();
 let template = parserHTML.parseFromString(templateString, 'text/html').body.firstChild;
 
-     fetch('https://my-json-server.typicode.com/shahin321/dbarticles/articles')
+     fetch('https://my-json-server.typicode.com/shahin321/db-articles/articles')
 	.then(response => response.json())
 	.then(articles => {
 		if (articles){
@@ -13,17 +13,6 @@ let template = parserHTML.parseFromString(templateString, 'text/html').body.firs
 				clone.querySelector('[article-title]').innerText = article.title;
 				clone.querySelector('[article-content]').innerText = article.content;
 				clone.querySelector('[article-image]').setAttribute("src", article.image );
-				var input = document.createElement('input');
-				input.setAttribute("type", "hidden")
-				input.id = 'input_' + article.id ;
-				input.value = article.id ;
-				var button = document.createElement('input');
-				button.setAttribute('type', 'submit');
-				button.innerHTML = 'Lire plus';
-				button.addEventListener('click', function(){myFunction(article.id)});
-				document.querySelector('#articles').appendChild(clone);
-				document.querySelector('#articles').appendChild(input);
-				document.querySelector('#articles').appendChild(button);
 			}
 		}
 	});
